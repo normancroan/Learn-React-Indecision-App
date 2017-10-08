b37c476292a05de9ca327ee309f44b327e26f605
Norman Croan <ncroan@gmail.com>
2017-10-07 22:20:32 -0400

    initial

---
+++ public/scripts/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var template = React.createElement(
+  'h1',
+  null,
+  'Live realoding the entire directory baby'
+);
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
---
+++ src/scripts/app.js
@@ -0,0 +1,5 @@
+var template = <h1>Live realoding the entire directory baby</h1>;
+
+var appRoot = document.getElementById('app')
+
+ReactDOM.render(template, appRoot);
