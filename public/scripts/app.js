5bbc6bcc18277c6269e68167e1def154ff838dd9
Norman Croan <ncroan@gmail.com>
2017-10-27 16:36:35 -0400

    working with arrays in JSX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,87 @@
 'use strict';
 
-var user = {
-    name: 'Norman',
-    age: 'Old',
-    location: 'Goffstown'
+console.log('App is running');
+var appRoot = document.getElementById('app');
+
+var app = {
+    title: "Indecision App",
+    subtitle: "Put your life in the hands of a computer.",
+    options: []
 };
 
-var app = React.createElement('div', null);
+var generateList = function generateList() {
+    return app.options.map(function (e) {
+        return React.createElement(
+            'li',
+            { key: e },
+            e
+        );
+    });
+};
 
-var appRoot = document.getElementById('app');
-ReactDOM.render(app, appRoot);
+var onFormSubmit = function onFormSubmit(e) {
+    e.preventDefault();
+    var option = e.target.elements.option.value;
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
+        generateList();
+        render();
+    }
+    console.log(app.options);
+};
+
+var onRemoveAll = function onRemoveAll() {
+    app.options = [];
+    render();
+};
+
+var render = function render() {
+    var template = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            app.title
+        ),
+        app.subtitle && React.createElement(
+            'p',
+            null,
+            app.subtitle
+        ),
+        React.createElement(
+            'p',
+            null,
+            app.options.length > 0 ? 'Here are your options' : 'No options'
+        ),
+        React.createElement(
+            'p',
+            null,
+            app.options.length
+        ),
+        React.createElement(
+            'button',
+            { onClick: onRemoveAll },
+            'Remove All'
+        ),
+        React.createElement(
+            'ol',
+            null,
+            generateList()
+        ),
+        React.createElement(
+            'form',
+            { onSubmit: onFormSubmit },
+            React.createElement('input', { type: 'text', name: 'option' }),
+            React.createElement(
+                'button',
+                null,
+                'Add Option'
+            )
+        )
+    );
+    ReactDOM.render(template, appRoot);
+};
+
+render();
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,7 +1,51 @@
-const app = 
-    <div>
+console.log('App is running');
+const appRoot = document.getElementById('app');
 
-    </div>
+const app = {
+    title: "Indecision App",
+    subtitle: "Put your life in the hands of a computer.",
+    options: []
+};
 
-const appRoot = document.getElementById('app')
-ReactDOM.render(app,appRoot)
+const generateList = () => {
+    return app.options.map(e => <li key={e}>{e}</li>)
+}
+
+const onFormSubmit = (e) => {
+    e.preventDefault();
+    const option = e.target.elements.option.value;
+    if (option){
+        app.options.push(option);
+        e.target.elements.option.value = '';
+        generateList()
+        render()
+    }
+    console.log(app.options);
+};
+
+const onRemoveAll = () => {
+    app.options = []
+    render()
+}
+
+const render = () => {
+    const template = (
+        <div>
+            <h1>{app.title}</h1>
+            {app.subtitle && <p>{app.subtitle}</p>}
+            <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+            <p>{app.options.length}</p>
+            <button onClick={onRemoveAll}>Remove All</button>
+            <ol>
+            {generateList()}
+            </ol>
+            <form onSubmit={onFormSubmit}>
+                <input type="text" name="option"/>
+                <button>Add Option</button>
+            </form>
+        </div>
+    );
+    ReactDOM.render(template,appRoot);
+};
+
+render()
