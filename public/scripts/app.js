047c6761fffe2046ccd541a4c03d837a96a5e1dd
Norman Croan <ncroan@gmail.com>
2017-10-28 16:13:20 -0400

    Using props to pass information down through components

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -18,14 +18,18 @@ var IndecisionApp = function (_React$Component) {
     }
 
     _createClass(IndecisionApp, [{
-        key: 'render',
+        key: "render",
         value: function render() {
+            var title = "Indecision";
+            var subtitle = "Put your life in the hands of a computer";
+            var options = ['Thing one', 'Thing two', 'Thing three'];
+
             return React.createElement(
-                'div',
+                "div",
                 null,
-                React.createElement(Header, null),
+                React.createElement(Header, { title: title, subtitle: subtitle }),
                 React.createElement(Action, null),
-                React.createElement(Options, null),
+                React.createElement(Options, { options: options }),
                 React.createElement(AddOptions, null)
             );
         }
@@ -44,20 +48,20 @@ var Header = function (_React$Component2) {
     }
 
     _createClass(Header, [{
-        key: 'render',
+        key: "render",
         value: function render() {
             return React.createElement(
-                'div',
+                "div",
                 null,
                 React.createElement(
-                    'h1',
+                    "h1",
                     null,
-                    'Indecision'
+                    this.props.title
                 ),
                 React.createElement(
-                    'h2',
+                    "h2",
                     null,
-                    'Put your life in the hands of a computer'
+                    this.props.subtitle
                 )
             );
         }
@@ -76,15 +80,15 @@ var Action = function (_React$Component3) {
     }
 
     _createClass(Action, [{
-        key: 'render',
+        key: "render",
         value: function render() {
             return React.createElement(
-                'div',
+                "div",
                 null,
                 React.createElement(
-                    'button',
+                    "button",
                     null,
-                    'What should I do?'
+                    "What should I do?"
                 )
             );
         }
@@ -103,15 +107,21 @@ var Options = function (_React$Component4) {
     }
 
     _createClass(Options, [{
-        key: 'render',
+        key: "render",
         value: function render() {
             return React.createElement(
-                'div',
+                "div",
                 null,
-                'Options component here',
-                React.createElement(Option, null),
-                React.createElement(Option, null),
-                React.createElement(Option, null)
+                "We've got ",
+                this.props.options.length,
+                " options",
+                React.createElement(
+                    "ol",
+                    null,
+                    this.props.options.map(function (option) {
+                        return React.createElement(Option, { option: option });
+                    })
+                )
             );
         }
     }]);
@@ -129,12 +139,12 @@ var Option = function (_React$Component5) {
     }
 
     _createClass(Option, [{
-        key: 'render',
+        key: "render",
         value: function render() {
             return React.createElement(
-                'div',
-                null,
-                'Option here'
+                "li",
+                { key: this.props.option },
+                this.props.option
             );
         }
     }]);
@@ -152,12 +162,12 @@ var AddOptions = function (_React$Component6) {
     }
 
     _createClass(AddOptions, [{
-        key: 'render',
+        key: "render",
         value: function render() {
             return React.createElement(
-                'div',
+                "div",
                 null,
-                'AddOptions component here'
+                "AddOptions component here"
             );
         }
     }]);
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,10 +1,14 @@
 class IndecisionApp extends React.Component {
     render() {
+        const title = "Indecision"
+        const subtitle = "Put your life in the hands of a computer"
+        const options = ['Thing one','Thing two','Thing three']
+
         return (
             <div>
-                <Header />
+                <Header title={title} subtitle={subtitle}/>
                 <Action />
-                <Options />
+                <Options options={options}/>
                 <AddOptions />
             </div>
         )
@@ -15,8 +19,8 @@ class Header extends React.Component {
     render() {
         return (
             <div>
-                <h1>Indecision</h1>
-                <h2>Put your life in the hands of a computer</h2>
+                <h1>{this.props.title}</h1>
+                <h2>{this.props.subtitle}</h2>
             </div>
         )
     }
@@ -36,10 +40,10 @@ class Options extends React.Component {
     render() {
         return (
             <div>
-                Options component here
-                <Option />
-                <Option />
-                <Option />
+                 We've got {this.props.options.length} options
+                <ol>
+                    {this.props.options.map(option =>  <Option option={option} />)}
+                </ol>
             </div>
         )
     }
@@ -48,7 +52,7 @@ class Options extends React.Component {
 class Option extends React.Component {
     render() {
         return (
-            <div>Option here</div>
+            <li key={this.props.option}>{this.props.option}</li>
         )
     }
 }
