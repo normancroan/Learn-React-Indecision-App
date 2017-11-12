6b4ae87cb0a0383cbab502492ea003b45bfa0576
Norman Croan <ncroan@gmail.com>
2017-11-12 18:36:00 -0500

    stlying options area

--- src/components/Options.js
+++ src/components/Options.js
@@ -3,12 +3,15 @@ import Option from './Option'
 
 const Options = (props) => (
     <div>
-        <button 
-            onClick={props.handleDeleteOptions}
-            className="button button--link"
-        >
-        Remove All</button>
-        {props.options.length === 0 && <p>Go ahead, add a few options to get started.</p>}
+        <div className="widget-header">
+            <h3 className="widget-header__title">Your Options</h3>
+                <button 
+                    onClick={props.handleDeleteOptions}
+                    className="button button--link"
+                >
+                Remove All</button>
+        </div>
+        {props.options.length === 0 && <p className="widget-message">Go ahead, add a few options to get started.</p>}
         <ol>
             {props.options.map(option =>  <Option option={option} handleDeleteOption={props.handleDeleteOption}/>)}
         </ol>
